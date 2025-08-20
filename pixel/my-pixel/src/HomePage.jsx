c7e5a118316c50ed71aa84006ffd845fe770735a
swapnakt <swapnakt103@gmail.com>
2025-08-20 15:03:46 +0530

    minor changes

---
+++ pixel/my-pixel/src/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <p className="bg-blue-100">hello my name is swapna</p>
+    </div>
+  );
+};
+
+export default HomePage;
